e9a7c396705e8fe8d8b53c9dfb116cf92fe61b59
Yahor Mishyn <yahor.mishyn@globallogic.com>
2023-04-28 15:14:57 +0200

    mongodb implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TourModule } from './tour/tour.module';
+import {MongooseModule} from "@nestjs/mongoose";
+import {getMongoConfig} from "./configs/mongo.config";
+
+
 
 @Module({
-	imports: [AuthModule, TourModule],
+	imports: [
+		MongooseModule.forRootAsync({
+			imports:[ConfigModule],
+			inject:[ConfigService],
+			useFactory: getMongoConfig
+		}),
+		ConfigModule.forRoot(),
+		AuthModule,
+		TourModule
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,9 @@ import {AuthDto} from "./dto/auth.dto";
 
 @Controller('auth')
 export class AuthController {
+    constructor() {
+        
+    }
 	@Post('register')
 	async register(@Body() dto: AuthDto) {
 
